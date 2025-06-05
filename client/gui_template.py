cd2a580990445830a4c82d77bf1053161a199422
Jiguli <jiguli.344a@gmail.com>
2025-06-05 09:25:04 +0300

    workpiece for doing oop

--- client/gui_template.py
+++ client/gui_template.py
@@ -7,16 +7,18 @@ dpg.create_context()
 dpg.create_viewport(title='Messanger', width=WIDTH // 2, height=HEIGHT // 2)
 
 with dpg.window(label="Messanger", width=dpg.get_viewport_width() // 2,
-                height=dpg.get_viewport_height(), tag="messanger"):
+                height=dpg.get_viewport_height() - 39, tag="messanger", no_title_bar=True):
     dpg.add_text("School chat")
     dpg.add_button(label="Send", )
     dpg.add_input_text(label="ur message", default_value="bobr curwa")
     dpg.add_slider_float(label="sm settings", default_value=0.853, max_value=1)
+previous_size = (dpg.get_viewport_width(), dpg.get_viewport_height())
 
 
 def resize_handler():
     width, height = dpg.get_viewport_width(), dpg.get_viewport_height()
-    dpg.configure_item("messanger", width=width // 2, height=height)
+    if previous_size != (width, height):
+        dpg.configure_item("messanger", width=width // 2, height=height - 39)
 
 
 dpg.setup_dearpygui()

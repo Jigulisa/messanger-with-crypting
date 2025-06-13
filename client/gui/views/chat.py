d5842194469362221ed875e471996f70b6b78984
Jigulisa <jiguli.344a@gmail.com>
2025-06-13 20:43:47 +0300

    tuple instead of list #11

--- client/gui/views/chat.py
+++ client/gui/views/chat.py
@@ -16,37 +16,37 @@ class Chat(View):
 
     def create_list(self) -> None:
         with child_window(label="Chats", width=get_viewport_client_width() // 4,
-                          pos=[0, 0],
+                          pos=(0, 0),
                           tag="chats_list"):
-            chats = ["andy", "class chagt", "dad", "barotrauma"]
+            chats = ("andy", "class chagt", "dad", "barotrauma")
             for chat in chats:
                 add_button(label=chat, tag=chat, callback=lambda f="sm1": self.callback(f, "chat_place"))
 
     def create_chat_place(self) -> None:
         with child_window(label="Messages", width=get_viewport_client_width() * 0.5,
                           height=get_viewport_client_height() * 0.65,
-                          pos=[get_viewport_client_width() * 0.25, get_viewport_client_height() * 0.1],
+                          pos=(get_viewport_client_width() * 0.25, get_viewport_client_height() * 0.1),
                           tag="chat_place"):
             add_text("chat place lmao")
 
     def create_text_zone(self) -> None:
         with child_window(label="text", width=get_viewport_client_width() * 0.5,
                           height=get_viewport_client_height() * 0.1,
-                          pos=[get_viewport_client_width() * 0.25, get_viewport_client_height() * 0.73],
+                          pos=(get_viewport_client_width() * 0.25, get_viewport_client_height() * 0.73),
                           tag="text_place"):
             add_input_text(default_value="☆*:.｡.o(≧▽≦)o.｡.:*☆ ", tag="input")
             add_button(label="send", callback=self.on_sending)
 
     def create_personal_zone(self):
         with child_window(width=get_viewport_client_width() * 0.5, height=get_viewport_client_height() * 0.1,
-                          pos=[get_viewport_client_width() // 4, 0], tag="personal_zone"):
+                          pos=(get_viewport_client_width() // 4, 0), tag="personal_zone"):
             add_text("no1", tag="chat_name")
 
     def callback(self, name, parent) -> None:
         delete_item(parent, children_only=True)
         with child_window(width=get_viewport_client_width() * 0.5,
                           height=get_viewport_client_width(),
-                          parent=parent, pos=[0, 0]):
+                          parent=parent, pos=(0, 0)):
             add_text(f"chat with {name}")
         set_value("chat_name", name)
 

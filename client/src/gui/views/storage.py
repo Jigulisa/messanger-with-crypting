d79bf7a1ba2d6e89b542ea1c1589b5578333753b
Jigulisa <jiguli.344a@gmail.com>
2025-08-11 11:24:25 +0300

    fixed generating menues in storage

--- client/src/gui/views/storage.py
+++ client/src/gui/views/storage.py
@@ -3,6 +3,8 @@ from dearpygui.dearpygui import (
     add_button,
     does_item_exist,
     delete_item,
+    get_item_label,
+    show_item,
     window)
 
 from gui.views.core import View
@@ -18,12 +20,17 @@ class Storage(View):
         for fn in self.file_names:
             add_button(label=fn, width=100, height=100, callback=self.on_tapping)
     
-    def on_tapping(self):
-        with window(tag="second_window", label="More...", width=300, height=300, no_title_bar=True):
-            add_button(label="download")
-            add_button(label="change name")
-            add_button(label="properties")
-            add_button(label="delete file")
-            add_button(label="close", callback=lambda: delete_item("second_window"))
+    def on_tapping(self, name: str):
+        if does_item_exist(f"second_window_{name}"):
+            delete_item(f"second_window_{name}")
+        self.menu = window(tag=f"second_window_{name}", width=300, height=300, no_title_bar=True, no_resize=True)
+        with self.menu:
+            add_text(default_value=str(get_item_label(name)))
+            add_button(label="download", width=285, height=30)
+            add_button(label="change name", width=285, height=30)
+            add_button(label="properties", width=285, height=30)
+            add_button(label="delete file", width=285, height=30)
+            add_button(label="close", callback=lambda: delete_item(f"second_window_{name}"), width=285, height=30)
+
 
 

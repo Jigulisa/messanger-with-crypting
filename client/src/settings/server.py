c6c5fc7cbc978dd2dc8841045a9e760b4f68ff3c
Jigulisa <jiguli.344a@gmail.com>
2025-08-12 14:23:20 +0300

    refactor: Refactored server settings

--- client/src/settings/server.py
+++ client/src/settings/server.py
@@ -23,9 +23,9 @@ class ServerMixin:
         return "ws"
 
     @staticmethod
-    def get_server_url() -> str:
-        return f"{ServerMixin.get_server_http_protocol()}://{ServerMixin.get_server_host()}/"
+    def get_server_url(postfix: str = "") -> str:
+        return f"{ServerMixin.get_server_http_protocol()}://{ServerMixin.get_server_host()}/{postfix}"
 
     @staticmethod
-    def get_server_storage_url() -> str:
-        return f"{ServerMixin.get_server_url()}storage/"
+    def get_server_storage_url(postfix: str = "") -> str:
+        return ServerMixin.get_server_url(f"storage/{postfix}")

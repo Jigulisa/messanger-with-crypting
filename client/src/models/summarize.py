8a96861f994140ca122591eba4cda8ca2d5ca295
Alya <145596958+Jigulisa@users.noreply.github.com>
2025-08-26 11:43:55 +0300

    Updated summariser

--- client/src/models/summarize.py
+++ client/src/models/summarize.py
@@ -1,12 +1,21 @@
 import torch
 from transformers import GPT2Tokenizer, T5ForConditionalGeneration
+from settings import Settings
+from settings.storage import Storage
 
+path = Storage.base_dir / ".cache/"
+model_name = "RussianNLP/FRED-T5-Summarizer"
+if not (path / model_name).exists():
+    ...
 tokenizer = GPT2Tokenizer.from_pretrained(
     "RussianNLP/FRED-T5-Summarizer",
     eos_token="</s>",  # noqa: S106
+    token=Settings.get_hf_token(),
 )
-model = T5ForConditionalGeneration.from_pretrained("RussianNLP/FRED-T5-Summarizer")
-
+model = T5ForConditionalGeneration.from_pretrained(
+    "RussianNLP/FRED-T5-Summarizer",
+    token=Settings.get_hf_token(),
+)
 
 def summarizer(data: str) -> str:
     input_text = f"<LM> Сократи текст.\n {data}"

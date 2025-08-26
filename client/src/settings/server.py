dccced96fff73ca898e99f2ad2b61f3cb99cca1b
Jigulisa <jiguli.344a@gmail.com>
2025-08-26 16:57:27 +0300

    feat: Added some server settings

--- client/src/settings/server.py
+++ client/src/settings/server.py
@@ -28,21 +28,24 @@ class ServerMixin:
 
     @staticmethod
     def get_server_http_url(postfix: str = "") -> str:
-        return f"{ServerMixin.get_server_http_protocol()}://{ServerMixin.get_server_host()}:{ServerMixin.get_server_port()}/{postfix}"
+        return f"{ServerMixin.get_server_http_protocol()}://{ServerMixin.get_server_host()}:{ServerMixin.get_server_port()}{postfix}"
 
     @staticmethod
     def get_server_ws_url(postfix: str = "") -> str:
-        return f"{ServerMixin.get_server_ws_protocol()}://{ServerMixin.get_server_host()}:{ServerMixin.get_server_port()}/{postfix}"
+        return f"{ServerMixin.get_server_ws_protocol()}://{ServerMixin.get_server_host()}:{ServerMixin.get_server_port()}{postfix}"
 
     @staticmethod
     def get_server_storage_url(postfix: str = "") -> str:
-        return ServerMixin.get_server_http_url(f"files/{postfix}")
+        return ServerMixin.get_server_http_url(f"/files{postfix}")
 
     @staticmethod
     def get_server_messages_url(postfix: str = "") -> str:
-        return ServerMixin.get_server_ws_url(f"messages/{postfix}")
+        return ServerMixin.get_server_ws_url(f"/messages{postfix}")
 
     @staticmethod
-    def get_auth_headers() -> dict[str, str]:
-        return {}
+    def get_server_chat_url(postfix: str = "") -> str:
+        return ServerMixin.get_server_http_url(f"/messages/chat{postfix}")
 
+    @staticmethod
+    def get_server_ai_url(postfix: str = "") -> str:
+        return ServerMixin.get_server_http_url(f"/ai{postfix}")
